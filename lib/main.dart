1e34b98c883532f123eff8d96ffe913fc9a4c50d
mwaidesu <machariamwai@gmail.com>
2023-10-17 18:32:26 +0300

    Add notes UI and strike through on checkbox change

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:noted/pages/first_page.dart';
 import 'package:noted/pages/home_page.dart';
-import 'package:noted/pages/profile_page.dart';
-import 'package:noted/pages/settings_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
@@ -13,14 +9,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: FirstPage(),
-      routes: {
-        '/homepage': (context) => HomePage(),
-        '/settingspage': (context) => SettingsPage(),
-        '/profilepage':(context) => ProfilePage(),
-      },
+    return SafeArea(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+        theme: ThemeData(primarySwatch: Colors.yellow),
+      ),
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,15 +1,45 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:noted/util/todo_tile.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ["Make Tutorial", false],
+    ["Exercise", false],
+  ];
+
+  //checkbox tapped
+  void checkBoxChanged(bool? value, int index) {
+    setState(() {
+      toDoList[index][1] = !toDoList[index][1];
+    }); 
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home Page")
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        title: Text("noted"),
+        elevation: 0,
+      ),
+      body: ListView.builder(
+        itemCount: toDoList.length,
+        itemBuilder: (context, index) {
+          return ToDoTile(
+            taskName: toDoList[index][0],
+            taskCompleted: toDoList[index][1],
+            onChanged: (value) => checkBoxChanged(value, index),
+          );
+        },
       ),
     );
   }
