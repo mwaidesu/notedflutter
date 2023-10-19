45386dac737b48a3e83c7b059b8f4e7ecfebe963
mwaidesu <machariamwai@gmail.com>
2023-10-19 18:22:58 +0300

    Display newly added note

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:noted/util/dialog_box.dart';
 import 'package:noted/util/todo_tile.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,16 +12,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+//controller
+  final _controller = TextEditingController();
+
   List toDoList = [
-    ["Make Tutorial", false],
-    ["Exercise", false],
+    ["Create To Do List", false],
+    ["Eat Pizza", false],
   ];
 
   //checkbox tapped
   void checkBoxChanged(bool? value, int index) {
     setState(() {
       toDoList[index][1] = !toDoList[index][1];
-    }); 
+    });
+  }
+
+  //save
+  void saveNewTask() {
+    setState(() {
+      toDoList.add([_controller.text, false]);
+      _controller.clear();
+    });
+    Navigator.of(context).pop();
+  }
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onSave: saveNewTask,
+          onCancel: () => Navigator.of(context).pop(),
+        );
+      },
+    );
   }
 
   @override
@@ -30,6 +56,10 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("noted"),
         elevation: 0,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: Icon(Icons.add),
       ),
       body: ListView.builder(
         itemCount: toDoList.length,
---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+import 'package:noted/util/my_button.dart';
+
+class DialogBox extends StatelessWidget {
+  final controller;
+  VoidCallback onSave;
+  VoidCallback onCancel;
+
+DialogBox({
+    super.key,
+    required this.controller,
+    required this.onSave,
+    required this.onCancel,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      backgroundColor: Colors.yellow[300],
+      content: Container(
+        height: 120,
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(), hintText: "Add a new task"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MyButton(text: "Save", onPressed: onSave),
+              MyButton(text: "Cancel", onPressed: onCancel),
+            ],
+          )
+        ]),
+      ),
+    );
+  }
+}
